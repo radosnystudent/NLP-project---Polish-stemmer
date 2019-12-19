c46901efc0b5ec8f268d60e962f603ac67c413f5
radosnystudent <jeremiasz.pauszek@gmail.com>
2019-12-19 12:05:32 +0100

    changes to module 'rules'

--- stemmer.py
+++ stemmer.py
@@ -1,27 +1,38 @@
-import rules as rs
+from rules import check_rule
 
 word = input("Podaj wyraz w języku polskim:\n> ")
 word = word.lower()
 
 changed = True
 
+all_checks = [
+        'plural suffix',
+        'diminutive suffix',
+        'nouns suffix',
+        'verbs suffix',
+        'verbs prefix',
+        'numerals suffix',
+        'infinitive suffix',
+        'adjective prefix',
+        'adjective suffix',
+        'adverbs suffix',
+        'general suffix'
+]
+
 while changed:
     changed = False
-    for rule in rs.rules.keys():
-        if len(word) > 5:
-            check, changes = rs.rules[rule](word)
-            if check != word:
-                if changes[1] == '':
-                    str_changes = '-' + changes[0]
-                else:
-                    str_changes = changes[1] + '-\n' + '-' + changes[0]
-                print(f'rule: {rule}\nword: {word} -> {check}\nchanges:\n{str_changes}\n------------')
+    for rule in all_checks:
+        if len(word) > 4:
+            check, changes = check_rule(word, rule)
+            if check != word and check:
+                print(f'rule: {rule}\nword: {word} -> {check}\ncutted:'
+                      f'{changes}\n------------')
                 word = check
                 changed = True
         else:
             break
-    if not changed or len(word) <= 5:
+    if not changed or len(word) <= 4:
         break
-    
-    
+
+
 print(f'rdzeń: {word}')
